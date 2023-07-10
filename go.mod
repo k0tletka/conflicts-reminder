12c8ac0ae06291e4862f87247794890303f52660
Nikita Gorbunov <k0tletka@thunix.net>
2023-07-10 12:19:43 +0300

    Added test

--- go.mod
+++ go.mod
@@ -3,7 +3,17 @@ module gitlab.tubecorporate.com/dsp-proxy/conflicts-reminder
 go 1.20
 
 require (
-	github.com/AlekSi/pointer v1.2.0 // indirect
+	github.com/AlekSi/pointer v1.2.0
+	github.com/magefile/mage v1.15.0
+	github.com/slack-go/slack v0.12.2
+	github.com/spf13/viper v1.16.0
+	github.com/stretchr/testify v1.8.3
+	github.com/xanzy/go-gitlab v0.86.0
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
@@ -11,21 +21,18 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/magefile/mage v1.15.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
-	github.com/slack-go/slack v0.12.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.16.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	github.com/xanzy/go-gitlab v0.86.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/oauth2 v0.10.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
--- internal/messager/messager_test.go
+++ internal/messager/messager_test.go
@@ -1,7 +1,75 @@
 package messager
 
-import "testing"
+import (
+	"context"
+	"github.com/slack-go/slack"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gitlab.tubecorporate.com/dsp-proxy/conflicts-reminder/internal/config"
+	"gitlab.tubecorporate.com/dsp-proxy/conflicts-reminder/internal/messager/mocks"
+	"testing"
+)
+
+const (
+	conflicts2result = `При проверке были обнаружены следующие конфликты💩:
+    <@UG29420>: конфликт в ветке master: <https://mrlink.1|Sample merge request>
+    <@UG29550>: конфликт в ветке developer-1: <https://mrlink.2|Sample merge request to developer branch>
+
+Кто до завтра их не исправит, тот крокодил🐊
+`
+)
 
 func TestSlackMessageSender_SendMessageWithConflictsData(t *testing.T) {
+	tests := []struct {
+		name        string
+		messageData *SlackConflictsData
+		cfg         *config.Config
+		setupMock   func(*mocks.SlackClient)
+	}{
+		{
+			name: "2 conflicts",
+			messageData: &SlackConflictsData{
+				Rows: []SlackConflictData{
+					{
+						SlackID:           "UG29490",
+						BranchName:        "master",
+						MergeRequestTitle: "Sample merge request",
+						MergeRequestLink:  "https://mrlink.1",
+					},
+					{
+						SlackID:           "UG29550",
+						BranchName:        "developer-1",
+						MergeRequestTitle: "Sample merge request to developer branch",
+						MergeRequestLink:  "https://mrlink.2",
+					},
+				},
+			},
+			cfg: &config.Config{
+				Slack: config.SlackConfig{
+					NotificationChannelID: "1234",
+				},
+			},
+			setupMock: func(m *mocks.SlackClient) {
+				m.On("SendMessageContext",
+					mock.Anything,
+					"1234",
+					slack.MsgOptionText(conflicts2result, false),
+				).Return("", "", "", nil)
+			},
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			clientMock := mocks.NewSlackClient(t)
+			slackClient := &SlackMessageSender{
+				cfg: tt.cfg,
+				api: clientMock,
+			}
+
+			tt.setupMock(clientMock)
+			err := slackClient.SendMessageWithConflictsData(context.Background(), tt.messageData)
+			assert.NoError(t, err)
+		})
+	}
 }
